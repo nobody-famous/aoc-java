49b7eac10883265ac29a8b4d90220a7e9750ee55
Rich Heller <rellehr@gmail.com>
2021-04-02 11:04:29 -0500

    Part 2 solution

--- src/y2020/day12/Part1.java
+++ src/y2020/day12/Part1.java
@@ -1,39 +1,35 @@
 package y2020.day12;
 
-public class Part1 {
-    private int x;
-    private int y;
-    private int dir;
-
-    private void north(int value) {
+public class Part1 extends Solver {
+    protected void north(int value) {
         y += value;
     }
 
-    private void south(int value) {
+    protected void south(int value) {
         y -= value;
     }
 
-    private void east(int value) {
+    protected void east(int value) {
         x += value;
     }
 
-    private void west(int value) {
+    protected void west(int value) {
         x -= value;
     }
 
-    private void left(int value) {
+    protected void left(int value) {
         var turns = value / 90;
 
         dir = (dir + (4 - turns)) % 4;
     }
 
-    private void right(int value) {
+    protected void right(int value) {
         var turns = value / 90;
 
         dir = (dir + turns) % 4;
     }
 
-    private void forward(int value) {
+    protected void forward(int value) {
         switch (dir) {
         case 0:
             y += value;
@@ -48,48 +44,6 @@ public class Part1 {
             x -= value;
             break;
         }
-    }
-
-    private void process(Instruction instr) {
-        switch (instr.getAction()) {
-        case 'N':
-            north(instr.getValue());
-            break;
-        case 'S':
-            south(instr.getValue());
-            break;
-        case 'E':
-            east(instr.getValue());
-            break;
-        case 'W':
-            west(instr.getValue());
-            break;
-        case 'L':
-            left(instr.getValue());
-            break;
-        case 'R':
-            right(instr.getValue());
-            break;
-        case 'F':
-            forward(instr.getValue());
-            break;
-        }
-    }
-
-    private long distance() {
-        return Math.abs(x) + Math.abs(y);
-    }
-
-    public long solve(Instruction[] instrs) {
-        x = 0;
-        y = 0;
-        dir = 1;
-
-        for (var instr : instrs) {
-            process(instr);
-        }
-
-        return distance();
     }
 
     public static void main(String[] args) {
---
+++ src/y2020/day12/Solver.java
@@ -0,0 +1,63 @@
+package y2020.day12;
+
+public abstract class Solver {
+    protected int x;
+    protected int y;
+    protected int dir;
+
+    abstract void north(int value);
+
+    abstract void south(int value);
+
+    abstract void east(int value);
+
+    abstract void west(int value);
+
+    abstract void left(int value);
+
+    abstract void right(int value);
+
+    abstract void forward(int value);
+
+    protected void process(Instruction instr) {
+        switch (instr.getAction()) {
+        case 'N':
+            north(instr.getValue());
+            break;
+        case 'S':
+            south(instr.getValue());
+            break;
+        case 'E':
+            east(instr.getValue());
+            break;
+        case 'W':
+            west(instr.getValue());
+            break;
+        case 'L':
+            left(instr.getValue());
+            break;
+        case 'R':
+            right(instr.getValue());
+            break;
+        case 'F':
+            forward(instr.getValue());
+            break;
+        }
+    }
+
+    protected long distance(int x, int y) {
+        return Math.abs(x) + Math.abs(y);
+    }
+
+    public long solve(Instruction[] instrs) {
+        x = 0;
+        y = 0;
+        dir = 1;
+
+        for (var instr : instrs) {
+            process(instr);
+        }
+
+        return distance(x, y);
+    }
+}
