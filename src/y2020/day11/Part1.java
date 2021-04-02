d86ca5ee26c4d69ba9ba8c62e6fa7b968145ce5a
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-04-01 22:06:26 -0500

    Day11 (#5)
    * Part 1 solution
    
    * Part 2 solution

---
+++ src/y2020/day11/Part1.java
@@ -0,0 +1,40 @@
+package y2020.day11;
+
+public class Part1 extends Solver {
+    public Part1(char[][] board) {
+        super(board);
+    }
+
+    protected int countNeighbors(int row, int col) {
+        int count = 0;
+
+        for (var nRow = row - 1; nRow <= row + 1; nRow += 1) {
+            for (var nCol = col - 1; nCol <= col + 1; nCol += 1) {
+                if (nRow == row && nCol == col) {
+                    continue;
+                }
+
+                if (lookup(nRow, nCol) == '#') {
+                    count += 1;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public long solve() {
+        while (!isDone()) {
+            doRound(4);
+        }
+
+        return countOccupied();
+    }
+
+    public static void main(String[] args) {
+        var solver = new Part1(Input.puzzle);
+        var answer = solver.solve();
+
+        System.out.println(answer);
+    }
+}
