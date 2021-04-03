b07663403e3d0be262185b2372b6fe81d9e43daa
Rich Heller <rellehr@gmail.com>
2021-04-02 22:13:17 -0500

    Part 2 solution

--- src/y2020/day13/Part2.java
+++ src/y2020/day13/Part2.java
@@ -1,52 +1,79 @@
 package y2020.day13;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Part2 {
-    public long solve(Notes input) {
-        var firstStep = 7;
-        var secondStep = 13;
-        var thirdStep = 59;
-        var fourthStep = 31;
-        var fifthStep = 19;
+    private long value;
+    private long step;
 
-        var first = firstStep;
-        var second = secondStep;
-        var third = thirdStep;
-        var fourth = fourthStep;
-        var fifth = fifthStep;
+    private List<Bus> notesToBuses(Notes input) {
+        var buses = new ArrayList<Bus>();
+        var offset = 0;
 
-        for (var loop = 0; loop < 10000000; loop += 1) {
-            if (second == (first + 1) && third == (first + 4) && fourth == (first + 6) && fifth == (first + 7)) {
-                System.out.println(first + " " + second);
+        for (var id : input.getIds()) {
+            if (id != null) {
+                buses.add(new Bus(id, offset));
             }
 
-            first += firstStep;
+            offset += 1;
+        }
 
-            if (first > second) {
-                second += secondStep;
-            }
-            if (second > third) {
-                third += thirdStep;
-            }
-            if (third > fourth) {
-                fourth += fourthStep;
-            }
-            if (fourth > fifth) {
-                fifth += fifthStep;
+        return buses;
+    }
+
+    private long sumIds(List<Bus> buses, int count) {
+        var sum = 1L;
+
+        for (var ndx = 0; ndx < count; ndx += 1) {
+            var bus = buses.get(ndx);
+            sum *= bus.getId();
+        }
+
+        return sum;
+    }
+
+    private boolean allMatch(List<Bus> buses, int count) {
+        for (var ndx = 1; ndx < count; ndx += 1) {
+            var bus = buses.get(ndx);
+            var target = value + bus.getOffset();
+
+            if (target % bus.getId() != 0) {
+                return false;
             }
         }
 
-        return 0;
+        return true;
+    }
+
+    private void findNextStep(List<Bus> buses, int count) {
+        while (true) {
+            value += step;
+
+            if (allMatch(buses, count)) {
+                step = sumIds(buses, count);
+                return;
+            }
+        }
+    }
+
+    public long solve(Notes input) {
+        var buses = notesToBuses(input);
+
+        value = buses.get(0).getId();
+        step = value;
+
+        for (var busCount = 2; busCount <= buses.size(); busCount += 1) {
+            findNextStep(buses, busCount);
+        }
+
+        return value;
     }
 
     public static void main(String[] args) {
         var solver = new Part2();
-        var answer = solver.solve(Input.sample);
+        var answer = solver.solve(Input.puzzle);
 
         System.out.println(answer);
     }
 }
-
-// When first and second line up,
-//   step = first times second
-//   new second = first x second
-//   new first = next number
