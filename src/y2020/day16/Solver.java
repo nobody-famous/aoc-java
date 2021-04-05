7fc4d708914ca6cc94c685678728797863aca15a
Rich Heller <rellehr@gmail.com>
2021-04-04 22:42:22 -0500

    Add helper function

--- src/y2020/day16/Solver.java
+++ src/y2020/day16/Solver.java
@@ -3,12 +3,20 @@ package y2020.day16;
 import java.util.List;
 
 public abstract class Solver {
+    protected boolean inField(long value, Field field) {
+        for (var range : field.getRanges()) {
+            if (value >= range.getLow() && value <= range.getHigh()) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     protected boolean isValid(long value, List<Field> fields) {
         for (var field : fields) {
-            for (var range : field.getRanges()) {
-                if (value >= range.getLow() && value <= range.getHigh()) {
-                    return true;
-                }
+            if (inField(value, field)) {
+                return true;
             }
         }
 
