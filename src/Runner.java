096c0cfdd44465003683024df40595408ad2f1bc
Rich Heller <rellehr@gmail.com>
2021-04-06 13:32:14 -0500

    Part 2 solution

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,8 @@ import utils.Problem;
 
 public class Runner {
     public static Problem[] allDays = new Problem[] { new y2020.day16.Part1(y2020.day16.Input.puzzle),
-            new y2020.day16.Part2(y2020.day16.Input.puzzle), };
+            new y2020.day16.Part2(y2020.day16.Input.puzzle), new y2020.day17.Part1(y2020.day17.Input.puzzle),
+            new y2020.day17.Part2(y2020.day17.Input.puzzle) };
 
     public void runAll(Problem[] all) {
         var total = 0L;
@@ -23,8 +24,8 @@ public class Runner {
 
     public static void main(String[] args) {
         var runner = new Runner();
-        // runner.runAll(allDays);
+        runner.runAll(allDays);
 
-        runner.runAll(new Problem[] { new y2020.day17.Part1(y2020.day17.Input.puzzle) });
+        // runner.runAll(new Problem[] { new y2020.day17.Part2(y2020.day17.Input.puzzle) });
     }
 }
