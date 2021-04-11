63e14a78d258efcb7d3949b3ac29181055c460af
Rich Heller <rellehr@gmail.com>
2021-04-10 23:20:21 -0500

    Part 1 solution

--- src/y2020/day20/Part1.java
+++ src/y2020/day20/Part1.java
@@ -3,6 +3,7 @@ package y2020.day20;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import utils.Problem;
 
@@ -27,16 +28,77 @@ public class Part1 implements Problem {
         return perms;
     }
 
-    public long solve() {
+    private void addBorder(Map<String, Integer> borders, String border) {
+        var value = 0;
+
+        if (borders.containsKey(border)) {
+            value = borders.get(border);
+        }
+
+        borders.put(border, value + 1);
+    }
+
+    private Map<String, Integer> bordersMap(Map<Integer, List<Tile>> tiles) {
+        var borders = new HashMap<String, Integer>();
+
+        for (var tileList : tiles.values()) {
+            for (var tile : tileList) {
+                addBorder(borders, tile.getNorth());
+                addBorder(borders, tile.getSouth());
+                addBorder(borders, tile.getEast());
+                addBorder(borders, tile.getWest());
+            }
+        }
+
+        return borders;
+    }
+
+    private Map<Integer, List<Tile>> tilePerms(Tile[] data) {
         var tiles = new HashMap<Integer, List<Tile>>();
 
-        for (var tile : input) {
+        for (var tile : data) {
             tiles.put(tile.getId(), permutations(tile));
         }
 
-        System.out.println(input[0]);
-        System.out.println(input[0].rotate());
+        return tiles;
+    }
 
+    private int countMatches(Map<String, Integer> borders, Tile tile) {
+        var count = 0;
+
+        if (borders.get(tile.getNorth()) > 4) {
+            count += 1;
+        }
+
+        if (borders.get(tile.getSouth()) > 4) {
+            count += 1;
+        }
+
+        if (borders.get(tile.getEast()) > 4) {
+            count += 1;
+        }
+
+        if (borders.get(tile.getWest()) > 4) {
+            count += 1;
+        }
+
+        return count;
+    }
+
+    public long solve() {
+        var tiles = tilePerms(input);
+        var borders = bordersMap(tiles);
+        var answer = 0L;
+
+        for (var tile : input) {
+            var count = countMatches(borders, tile);
+
+            if (count == 2) {
+                answer = (answer == 0) ? tile.getId() : answer * tile.getId();
+            }
+        }
+
+        System.out.println(answer);
         return 0;
     }
 }
