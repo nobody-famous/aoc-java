9a2cf39bde98847f3e08ad4f9b86471d9d6c1da7
Rich Heller <rellehr@gmail.com>
2021-04-13 14:24:22 -0500

    Input and data structures

---
+++ src/y2020/day21/Food.java
@@ -0,0 +1,6 @@
+package y2020.day21;
+
+public class Food {
+    public Food(String[] ingrs, String[] allergens) {
+    }
+}
---
+++ src/y2020/day21/Part1.java
@@ -0,0 +1,15 @@
+package y2020.day21;
+
+import utils.Problem;
+
+public class Part1 implements Problem {
+    private Food[] input;
+
+    public Part1(Food[] input) {
+        this.input = input;
+    }
+
+    public long solve() {
+        return 0;
+    }
+}
