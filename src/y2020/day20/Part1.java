097a2c35f7146bce2245728492ec07844bec99b0
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-04-12 23:29:02 -0500

    Day20 (#13)
    * Input and data structures
    
    * Part 1 solution
    
    * Start placing tiles
    
    * Code cleanup
    
    * Place tiles into a grid
    
    * Code cleanup
    
    * Part 2 solution

---
+++ src/y2020/day20/Part1.java
@@ -0,0 +1,23 @@
+package y2020.day20;
+
+public class Part1 extends Solver {
+    public Part1(Tile[] input) {
+        super(input);
+    }
+
+    public long solve() {
+        var tiles = tilePerms(input);
+        var borders = bordersMap(tiles);
+        var answer = 0L;
+
+        for (var tile : input) {
+            var count = countMatches(borders, tile);
+
+            if (count == 2) {
+                answer = (answer == 0) ? tile.getId() : answer * tile.getId();
+            }
+        }
+
+        return answer;
+    }
+}
