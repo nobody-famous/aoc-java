71a93b80b3acc3795a12f25e9901501d678eb871
Rich Heller <rellehr@gmail.com>
2021-04-16 23:00:08 -0500

    Day 25 solution

---
+++ src/y2020/day25/Input.java
@@ -0,0 +1,7 @@
+package y2020.day25;
+
+public interface Input {
+    int[] sample = new int[] { 17807724, 5764801 };
+
+    int[] puzzle = new int[] { 19774466, 7290641 };
+}
---
+++ src/y2020/day25/Part1.java
@@ -0,0 +1,36 @@
+package y2020.day25;
+
+import utils.Problem;
+
+public class Part1 implements Problem {
+    private int[] input;
+
+    public Part1(int[] input) {
+        this.input = input;
+    }
+
+    private long transform(long value, long subject) {
+        value *= subject;
+        value %= 20201227;
+
+        return value;
+    }
+
+    public long solve() {
+        var value = 1L;
+        var subject = 7L;
+        var loopSize = 0;
+
+        while (value != input[0]) {
+            value = transform(value, subject);
+            loopSize += 1;
+        }
+
+        value = 1L;
+        for (var loop = 0; loop < loopSize; loop += 1) {
+            value = transform(value, input[1]);
+        }
+
+        return value;
+    }
+}
