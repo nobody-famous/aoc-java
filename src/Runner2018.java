2e8f387a409585a5759122794c699ae6130c826a
Rich Heller <rellehr@gmail.com>
2021-08-10 17:55:41 -0500

    Day 2, part 1

--- src/Runner2018.java
+++ src/Runner2018.java
@@ -2,13 +2,14 @@ import utils.AocProblem;
 import utils.Runner;
 
 public class Runner2018 extends Runner {
-    public static AocProblem[] allDays = new AocProblem[] { new y2018.day1.Part1("input/2018/day1/puzzle.txt", 437L),
-            new y2018.day1.Part2("input/2018/day1/puzzle.txt", 655) };
+    public static AocProblem[] allDays = new AocProblem[] { new y2018.day1.Part1("input/2018/day1/puzzle.txt", 437),
+            new y2018.day1.Part2("input/2018/day1/puzzle.txt", 655),
+            new y2018.day2.Part1("input/2018/day2/puzzle.txt", 6225), };
 
     public static void main(String[] args) {
         var runner = new Runner2018();
 
-        // var allDays = new AocProblem[] { new y2018.day1.Part2("input/2018/day1/puzzle.txt", 655L) };
+        var allDays = new AocProblem[] { new y2018.day2.Part1("input/2018/day2/puzzle.txt", 6225) };
         runner.runAll(allDays);
     }
 }
--- src/y2018/day1/Part1.java
+++ src/y2018/day1/Part1.java
@@ -2,18 +2,18 @@ package y2018.day1;
 
 import utils.Problem;
 
-public class Part1 extends Problem<Long> {
+public class Part1 extends Problem<Integer> {
     private Parser parser;
 
-    public Part1(String fileName, Long exp) {
+    public Part1(String fileName, int exp) {
         super(exp);
 
         parser = new Parser(fileName);
     }
 
-    public Long run() {
+    public Integer run() {
         var ints = parser.parse();
-        var total = 0L;
+        var total = 0;
 
         for (var i : ints) {
             total += i;
