bc965e9468f17bd55b5efe5b774b003918ce5f9f
Rich Heller <rellehr@gmail.com>
2021-08-12 13:34:09 -0500

    Part 2

--- src/Runner2018.java
+++ src/Runner2018.java
@@ -5,12 +5,14 @@ public class Runner2018 extends Runner {
     public static AocProblem[] allDays = new AocProblem[] { new y2018.day1.Part1("input/2018/day1/puzzle.txt", 437),
             new y2018.day1.Part2("input/2018/day1/puzzle.txt", 655),
             new y2018.day2.Part1("input/2018/day2/puzzle.txt", 6225),
-            new y2018.day2.Part2("input/2018/day2/puzzle.txt", "revtaubfniyhsgxdoajwkqilp") };
+            new y2018.day2.Part2("input/2018/day2/puzzle.txt", "revtaubfniyhsgxdoajwkqilp"),
+            new y2018.day3.Part1("input/2018/day3/puzzle.txt", 118858),
+            new y2018.day3.Part2("input/2018/day3/puzzle.txt", 1100) };
 
     public static void main(String[] args) {
         var runner = new Runner2018();
 
-        var allDays = new AocProblem[] { new y2018.day3.Part1("input/2018/day3/puzzle.txt", 118858) };
+        // var allDays = new AocProblem[] { new y2018.day3.Part2("input/2018/day3/puzzle.txt", 1100) };
 
         runner.runAll(allDays);
     }
