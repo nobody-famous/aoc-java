7286b3ac4b1578e6b3b0d4a1aee257d97f22a6c0
Rich Heller <rellehr@gmail.com>
2021-08-25 22:57:11 -0500

    Part 2

--- src/Runner2019.java
+++ src/Runner2019.java
@@ -5,12 +5,14 @@ public class Runner2019 extends Runner {
     public static AocProblem[] allDays = new AocProblem[] { new y2019.day1.Part1("input/2019/day1/puzzle.txt", 3279287),
             new y2019.day1.Part2("input/2019/day1/puzzle.txt", 4916076),
             new y2019.day2.Part1("input/2019/day2/puzzle.txt", 3101844),
-            new y2019.day2.Part2("input/2019/day2/puzzle.txt", 8478) };
+            new y2019.day2.Part2("input/2019/day2/puzzle.txt", 8478),
+            new y2019.day3.Part1("input/2019/day3/puzzle.txt", 529),
+            new y2019.day3.Part2("input/2019/day3/puzzle.txt", 20386) };
 
     public static void main(String[] args) {
         var runner = new Runner2018();
 
-        var allDays = new AocProblem[] { new y2019.day3.Part1("input/2019/day3/puzzle.txt", 529) };
+        // var allDays = new AocProblem[] { new y2019.day3.Part2("input/2019/day3/puzzle.txt", 20386) };
 
         runner.runAll(allDays);
     }
--- src/y2019/day3/Part1.java
+++ src/y2019/day3/Part1.java
@@ -1,59 +1,15 @@
 package y2019.day3;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import utils.Problem;
-import utils.geometry.Line;
 import utils.geometry.Point;
 
-public class Part1 extends Problem<Integer> {
-    private Parser parser;
-
+public class Part1 extends Solver {
     public Part1(String fileName, int exp) {
-        super(exp);
-
-        parser = new Parser(fileName);
+        super(fileName, exp);
     }
 
-    private boolean isBetween(int value, int start, int end) {
-        return (start < end) ? value >= start && value <= end : value >= end && value <= start;
-    }
-
-    private boolean linesCross(Line vert, Line horiz) {
-        return isBetween(vert.start.x, horiz.start.x, horiz.end.x)
-                && isBetween(horiz.start.y, vert.start.y, vert.end.y);
-    }
-
-    private Point crossPoint(Line line1, Line line2) {
-        if (line1.start.x == line1.end.x && line2.start.y == line2.end.y) {
-            return linesCross(line1, line2) ? new Point(line1.start.x, line2.start.y) : null;
-        } else if (line2.start.x == line2.end.x && line1.start.y == line1.end.y) {
-            return linesCross(line2, line1) ? new Point(line2.start.x, line1.start.y) : null;
-        }
-
-        return null;
-    }
-
-    private List<Point> findCrosses(Wire[] wires) {
-        var crosses = new ArrayList<Point>();
-
-        for (var line1 : wires[0].lines) {
-            for (var line2 : wires[1].lines) {
-                var pt = crossPoint(line1, line2);
-
-                if (pt != null) {
-                    crosses.add(pt);
-                }
-            }
-        }
-
-        return crosses;
-    }
-
-    public Integer run() {
-        var wires = parser.parse();
-        var crosses = findCrosses(wires);
+    public int doWork(Wire[] wires, List<Point> crosses) {
         var closest = Integer.MAX_VALUE;
         var origin = new Point(0, 0);
 
