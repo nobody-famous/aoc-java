224b682df47c7a4c09e963b3caaba6c009fe8d9d
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-08-26 22:44:35 -0500

    Day4 (#24)
    * Part 1
    
    * Part 2

--- src/Runner2019.java
+++ src/Runner2019.java
@@ -7,12 +7,14 @@ public class Runner2019 extends Runner {
             new y2019.day2.Part1("input/2019/day2/puzzle.txt", 3101844),
             new y2019.day2.Part2("input/2019/day2/puzzle.txt", 8478),
             new y2019.day3.Part1("input/2019/day3/puzzle.txt", 529),
-            new y2019.day3.Part2("input/2019/day3/puzzle.txt", 20386) };
+            new y2019.day3.Part2("input/2019/day3/puzzle.txt", 20386),
+            new y2019.day4.Part1("input/2019/day4/puzzle.txt", 511),
+            new y2019.day4.Part2("input/2019/day4/puzzle.txt", 316) };
 
     public static void main(String[] args) {
         var runner = new Runner2018();
 
-        // var allDays = new AocProblem[] { new y2019.day3.Part2("input/2019/day3/puzzle.txt", 20386) };
+        // var allDays = new AocProblem[] { new y2019.day4.Part2("input/2019/day4/puzzle.txt", 316) };
 
         runner.runAll(allDays);
     }
---
+++ src/y2019/day4/Part1.java
@@ -0,0 +1,18 @@
+package y2019.day4;
+
+public class Part1 extends Solver {
+
+    public Part1(String fileName, int exp) {
+        super(fileName, exp);
+    }
+
+    protected boolean isValid(int[] num) {
+        for (var ndx = 1; ndx < num.length; ndx += 1) {
+            if (num[ndx] == num[ndx - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
