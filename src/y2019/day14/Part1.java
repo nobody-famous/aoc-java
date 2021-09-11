a677459073352dbdb061a87983158593b19f1490
Rich Heller <rellehr@gmail.com>
2021-09-10 22:34:38 -0500

    Part 1

--- src/y2019/day14/Part1.java
+++ src/y2019/day14/Part1.java
@@ -1,11 +1,7 @@
 package y2019.day14;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import utils.Problem;
 
@@ -19,103 +15,83 @@ public class Part1 extends Problem<Integer> {
         parser = new Parser(fileName);
     }
 
-    private Set<String> getFromOre() {
-        var fromOre = new HashSet<String>();
-
-        for (var entry : reactions.entrySet()) {
-            var react = entry.getValue();
-
-            if (react.input().size() == 1 && react.input().get(0).name().equals("ORE")) {
-                fromOre.add(entry.getKey());
-            }
-        }
-
-        return fromOre;
-    }
-
     private void addToMap(Map<String, Integer> dict, String name, int amount) {
         var existing = dict.containsKey(name) ? dict.get(name) : 0;
 
         dict.put(name, existing + amount);
     }
 
-    private void visit(Map<String, Integer> required, Map<String, Integer> pile, List<String> path, String name,
-            int mx) {
-        var react = reactions.get(name);
-
-        if (react == null) {
-            return;
-        }
-
-        for (var child : react.input()) {
-            var childReact = reactions.get(child.name());
-
-            if (childReact == null) {
-                continue;
-            }
-
-            var need = child.amount() * mx;
-            var make = childReact.output().amount();
-            var mult = getMultiplier(need, make);
-            var rem = (make * mult) - need;
-
-            if (rem > 0) {
-                addToMap(pile, child.name(), rem);
-            }
-
-            addToMap(required, child.name(), need);
-
-            visit(required, pile, path, child.name(), mult);
-        }
-    }
-
     private int getMultiplier(int need, int make) {
         return need % make == 0 ? need / make : (need / make) + 1;
     }
 
-    private List<String> buildPath() {
-        var path = new ArrayList<String>();
-        var fromOre = getFromOre();
-        var required = new HashMap<String, Integer>();
-        var pile = new HashMap<String, Integer>();
+    private Map<String, Integer> getChemicals(Map<String, Integer> pile, String name, int reqAmount) {
+        var react = reactions.get(name);
+        var chems = new HashMap<String, Integer>();
+        var mult = getMultiplier(reqAmount, react.output().amount());
+        var rem = (react.output().amount() * mult) - reqAmount;
 
-        visit(required, pile, path, "FUEL", 1);
-
-        var total = 0;
-        for (var name : fromOre) {
-            var react = reactions.get(name);
-            var fromPile = pile.containsKey(name) ? pile.get(name) : 0;
-            var req = required.get(name) - fromPile;
-            var ore = react.input().get(0).amount();
-            var mult = getMultiplier(req, react.output().amount());
-
-            total += (ore * mult);
+        if (rem > 0) {
+            addToMap(pile, name, rem);
         }
 
-        System.out.println("total ore " + total);
+        for (var chem : react.input()) {
+            chems.put(chem.name(), chem.amount() * mult);
+        }
 
-        return path;
+        return chems;
+    }
+
+    private int getFromPile(Map<String, Integer> pile, String name, int amount) {
+        if (!pile.containsKey(name)) {
+            return amount;
+        }
+
+        var inPile = pile.get(name);
+
+        if (inPile == amount) {
+            pile.remove(name);
+            return 0;
+        } else if (inPile > amount) {
+            addToMap(pile, name, -amount);
+            return 0;
+        }
+
+        pile.remove(name);
+
+        return amount - inPile;
+    }
+
+    private Map<String, Integer> getNextLevel(Map<String, Integer> pile, Map<String, Integer> curLevel) {
+        var nextLevel = new HashMap<String, Integer>();
+
+        for (var chem : curLevel.entrySet()) {
+            if ("ORE".equals(chem.getKey())) {
+                addToMap(nextLevel, chem.getKey(), chem.getValue());
+                continue;
+            }
+
+            var toGet = getFromPile(pile, chem.getKey(), chem.getValue());
+            var kids = getChemicals(pile, chem.getKey(), toGet);
+
+            for (var kid : kids.entrySet()) {
+                addToMap(nextLevel, kid.getKey(), kid.getValue());
+            }
+        }
+
+        return nextLevel;
     }
 
     public Integer run() {
         reactions = parser.parse();
+        var pile = new HashMap<String, Integer>();
+        Map<String, Integer> level = new HashMap<String, Integer>();
 
-        var path = buildPath();
-        System.out.println("path " + path);
+        level.put("FUEL", 1);
+        do {
+            level = getNextLevel(pile, level);
+        } while (level.size() > 1);
 
-        // var needed = (Map<String, Integer>) new HashMap<String, Integer>();
-
-        // needed.put("FUEL", reactions.get("FUEL").output().amount());
-
-        // while (!onlyOre(needed)) {
-        //     needed = updateNeeded(needed);
-        //     System.out.println(needed);
-        // }
-
-        // System.out.println("Needed " + needed);
-
-        // return needed.get("ORE");
-
-        return 0;
+        return level.get("ORE");
     }
 }
