c795279f950728e3d1957f0be97f87134d0f9871
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-10-09 15:33:22 -0500

    Day15 (#35)
    * Part 1 data structures
    
    * Create map of the grid
    
    * Code cleanup
    
    * Code cleanup
    
    * Part 1
    
    * Part 2

---
+++ src/y2019/day15/RepairDroid.java
@@ -0,0 +1,45 @@
+package y2019.day15;
+
+import y2019.intcode.Machine;
+
+public class RepairDroid implements Machine.IO {
+    public static final int North = 1;
+    public static final int South = 2;
+    public static final int West = 3;
+    public static final int East = 4;
+
+    public static final int Wall = 0;
+    public static final int Open = 1;
+    public static final int Oxygen = 2;
+
+    private Machine mach;
+    private Long nextInput;
+    private Long lastOutput;
+
+    public RepairDroid(RepairDroid clone) {
+        this.mach = new Machine(clone.mach, this);
+    }
+
+    public RepairDroid(long[] prog) {
+        this.mach = new Machine(prog, this);
+    }
+
+    public int move(int dir) {
+        nextInput = (long) dir;
+        lastOutput = null;
+
+        while (lastOutput == null) {
+            mach.exec();
+        }
+
+        return (int) lastOutput.longValue();
+    }
+
+    public long input() {
+        return nextInput;
+    }
+
+    public void output(long value) {
+        lastOutput = value;
+    }
+}
