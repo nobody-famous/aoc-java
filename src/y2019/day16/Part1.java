2e3d0c3deb9516d4f7c8ac56409122bde4e9e5fb
Rich Heller <rellehr@gmail.com>
2021-10-09 22:05:25 -0500

    Part 1

--- src/y2019/day16/Part1.java
+++ src/y2019/day16/Part1.java
@@ -1,5 +1,8 @@
 package y2019.day16;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import utils.Problem;
 
 public class Part1 extends Problem<Integer> {
@@ -11,11 +14,50 @@ public class Part1 extends Problem<Integer> {
         parser = new Parser(fileName);
     }
 
+    private int apply(List<Integer> input, int element) {
+        var startNdx = element - 1;
+        var total = 0;
+        var mult = 1;
+
+        while (startNdx < input.size()) {
+            for (var ndx = startNdx; ndx < input.size() && ndx < startNdx + element; ndx += 1) {
+                total += input.get(ndx) * mult;
+            }
+
+            startNdx += (element * 2);
+            mult = -mult;
+        }
+
+        return Math.abs(total % 10);
+    }
+
+    private List<Integer> applyPattern(List<Integer> input) {
+        var output = new ArrayList<Integer>();
+
+        for (var element = 1; element <= input.size(); element += 1) {
+            output.add(apply(input, element));
+        }
+
+        return output;
+    }
+
+    private int listToInt(List<Integer> list) {
+        var output = 0;
+
+        for (var item : list) {
+            output = (output * 10) + item;
+        }
+
+        return output;
+    }
+
     public Integer run() {
         var nums = parser.parse();
 
-        System.out.println(nums);
+        for (var loop = 0; loop < 100; loop += 1) {
+            nums = applyPattern(nums);
+        }
 
-        return 0;
+        return listToInt(nums.subList(0, 8));
     }
 }
