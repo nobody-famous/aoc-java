2138c32011e2041d04bdc26f48a8fe14113e6cf0
nobody-famous <38633692+nobody-famous@users.noreply.github.com>
2021-10-12 23:28:43 -0500

    Day17 (#37)
    * Part 1
    
    * Start part 2
    
    * Part 2

---
+++ src/y2019/day17/Part2.java
@@ -0,0 +1,143 @@
+package y2019.day17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import utils.Problem;
+import utils.geometry.Point;
+import y2019.intcode.Parser;
+
+public class Part2 extends Problem<Integer> {
+    private Parser parser;
+
+    public Part2(String fileName, int exp) {
+        super(exp);
+
+        parser = new Parser(fileName);
+    }
+
+    private boolean hasMove(Grid grid) {
+        var pt = grid.getRobot();
+        var scaffold = grid.getScaffold();
+
+        return switch (grid.getRobotDir()) {
+            case '^', 'v' -> scaffold.contains(new Point(pt.x + 1, pt.y))
+                    || scaffold.contains(new Point(pt.x - 1, pt.y));
+            case '>', '<' -> scaffold.contains(new Point(pt.x, pt.y + 1))
+                    || scaffold.contains(new Point(pt.x, pt.y - 1));
+            default -> false;
+        };
+    }
+
+    private int moveRobot(Grid grid, Point robot) {
+        var scaffold = grid.getScaffold();
+        var moves = 0;
+        var delta = switch (grid.getRobotDir()) {
+            case '^' -> new Point(0, -1);
+            case 'v' -> new Point(0, 1);
+            case '>' -> new Point(1, 0);
+            case '<' -> new Point(-1, 0);
+            default -> throw new RuntimeException("Unhandled direction: " + grid.getRobotDir());
+        };
+
+        var next = new Point(robot.x + delta.x, robot.y + delta.y);
+        while (scaffold.contains(next)) {
+            moves += 1;
+
+            robot.x = next.x;
+            robot.y = next.y;
+
+            next = new Point(robot.x + delta.x, robot.y + delta.y);
+        }
+
+        return moves;
+    }
+
+    private Movement getMove(Grid grid) {
+        var robot = grid.getRobot();
+        var scaffold = grid.getScaffold();
+        var turn = switch (grid.getRobotDir()) {
+            case '^' -> scaffold.contains(new Point(robot.x + 1, robot.y)) ? 'R' : 'L';
+            case 'v' -> scaffold.contains(new Point(robot.x - 1, robot.y)) ? 'R' : 'L';
+            case '>' -> scaffold.contains(new Point(robot.x, robot.y + 1)) ? 'R' : 'L';
+            case '<' -> scaffold.contains(new Point(robot.x, robot.y - 1)) ? 'R' : 'L';
+            default -> throw new RuntimeException("Unhandled direction: " + grid.getRobotDir());
+        };
+        var newDir = switch (grid.getRobotDir()) {
+            case '^' -> turn == 'R' ? '>' : '<';
+            case 'v' -> turn == 'R' ? '<' : '>';
+            case '>' -> turn == 'R' ? 'v' : '^';
+            case '<' -> turn == 'R' ? '^' : 'v';
+            default -> throw new RuntimeException("Unhandled direction: " + grid.getRobotDir());
+        };
+
+        grid.setRobotDir(newDir);
+
+        var moves = moveRobot(grid, robot);
+
+        return new Movement(turn, moves);
+    }
+
+    private List<Movement> buildPath(Grid grid) {
+        var moves = new ArrayList<Movement>();
+
+        while (hasMove(grid)) {
+            moves.add(getMove(grid));
+        }
+
+        return moves;
+    }
+
+    private void sendParts(Controller ctrl, PathParts parts) {
+        var sent = 0;
+
+        while (sent < 4) {
+            var line = ctrl.readLine();
+
+            if (line.equals("Main:")) {
+                ctrl.writeLine(parts.main());
+            } else if (line.equals("Function A:")) {
+                ctrl.writeLine(parts.A());
+            } else if (line.equals("Function B:")) {
+                ctrl.writeLine(parts.B());
+            } else if (line.equals("Function C:")) {
+                ctrl.writeLine(parts.C());
+            } else {
+                throw new RuntimeException("Unhandled line: " + line);
+            }
+
+            sent += 1;
+        }
+    }
+
+    private void answerPrompt(Controller ctrl) {
+        var line = ctrl.readLine();
+
+        if (!line.equals("Continuous video feed?")) {
+            throw new RuntimeException("Unhandled line: " + line);
+        }
+
+        ctrl.writeLine("n");
+    }
+
+    public Integer run() {
+        var prog = parser.parse();
+        var ctrl = new Controller(prog);
+
+        ctrl.wakeRobot();
+
+        var output = ctrl.readCamera();
+        var grid = Grid.fromCamera(output);
+        var path = buildPath(grid);
+        var splitter = new PathSplitter(path);
+        var parts = splitter.split();
+
+        sendParts(ctrl, parts);
+        answerPrompt(ctrl);
+
+        ctrl.readLine();
+        ctrl.readCamera();
+
+        return (int) ctrl.readValue();
+    }
+}
---
+++ src/y2019/day17/PathSplitter.java
@@ -0,0 +1,102 @@
+package y2019.day17;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSplitter {
+    private List<Movement> path;
+
+    public PathSplitter(List<Movement> path) {
+        this.path = path;
+    }
+
+    public PathParts split() {
+        var opts = buildOptions(path);
+
+        return traverse(opts, 0, new PathParts(null, null, null, null));
+    }
+
+    private PathParts updateParts(PathParts curParts, String opt) {
+        var main = curParts.main();
+        var A = curParts.A();
+        var B = curParts.B();
+        var C = curParts.C();
+
+        if (A == null) {
+            return new PathParts("A", opt, null, null);
+        } else if (opt.equals(A)) {
+            return new PathParts(main + ",A", A, B, C);
+        }
+
+        if (B == null) {
+            return new PathParts(main + ",B", A, opt, null);
+        } else if (opt.equals(B)) {
+            return new PathParts(main + ",B", A, B, C);
+        }
+
+        if (C == null) {
+            return new PathParts(main + ",C", A, B, opt);
+        } else if (opt.equals(C)) {
+            return new PathParts(main + ",C", A, B, C);
+        }
+
+        return null;
+    }
+
+    private PathParts traverse(List<List<String>> options, int optNdx, PathParts curParts) {
+        if (optNdx == options.size()) {
+            return curParts;
+        }
+
+        var curOpts = options.get(optNdx);
+
+        for (var ndx = 0; ndx < curOpts.size(); ndx += 1) {
+            var opt = curOpts.get(ndx);
+            var parts = updateParts(curParts, opt);
+
+            if (parts == null) {
+                continue;
+            }
+
+            parts = traverse(options, optNdx + ndx + 1, parts);
+            if (parts != null) {
+                return parts;
+            }
+        }
+
+        return null;
+    }
+
+    private String movementToOpt(Movement move) {
+        return "" + move.turn() + "," + move.distance();
+    }
+
+    private List<String> buildOptions(List<Movement> path, int ndx) {
+        var options = new ArrayList<String>();
+        var str = new StringBuilder();
+
+        str.append(movementToOpt(path.get(ndx)));
+        options.add(str.toString());
+        ndx += 1;
+        while (ndx < path.size() && str.length() < 20) {
+            str.append(",").append(movementToOpt(path.get(ndx)));
+            ndx += 1;
+
+            if (str.length() < 20) {
+                options.add(str.toString());
+            }
+        }
+
+        return options;
+    }
+
+    private List<List<String>> buildOptions(List<Movement> path) {
+        var options = new ArrayList<List<String>>();
+
+        for (var ndx = 0; ndx < path.size(); ndx += 1) {
+            options.add(buildOptions(path, ndx));
+        }
+
+        return options;
+    }
+}
