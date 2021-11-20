cfc1c052dc6b85839a7ea7ed6203eacdcee4e179
Rich Heller <rellehr@gmail.com>
2021-11-20 08:23:26 -0600

    Code cleanup

--- src/main/java/aoc/y2019/day22/Part2.java
+++ src/main/java/aoc/y2019/day22/Part2.java
@@ -25,57 +25,48 @@ public class Part2 extends Problem<Long> {
         var pos = startPos;
         var a = BigInteger.ONE;
         var b = BigInteger.ZERO;
-        var iter = BigInteger.ZERO;
 
         Collections.reverse(steps);
 
-        while (true) {
-            var oldPos = pos;
-            a = BigInteger.ONE;
-            b = BigInteger.ZERO;
+        var oldPos = pos;
+        a = BigInteger.ONE;
+        b = BigInteger.ZERO;
 
-            for (var step : steps) {
-                if (pos.compareTo(BigInteger.ZERO) < 0 || pos.compareTo(size) >= 0) {
-                    throw new RuntimeException("Invalid pos " + pos);
+        for (var step : steps) {
+            if (pos.compareTo(BigInteger.ZERO) < 0 || pos.compareTo(size) >= 0) {
+                throw new RuntimeException("Invalid pos " + pos);
+            }
+
+            if (step instanceof DealStack) {
+                pos = size.subtract(BigInteger.ONE).subtract(pos);
+                a = a.negate();
+                b = b.negate().add(size).subtract(BigInteger.ONE);
+            } else if (step instanceof CutCards) {
+                var cut = (CutCards) step;
+                var n = BigInteger.valueOf(cut.getNumCards());
+
+                if (n.compareTo(BigInteger.ZERO) < 0) {
+                    n = size.subtract(n.abs());
                 }
 
-                if (step instanceof DealStack) {
-                    pos = size.subtract(BigInteger.ONE).subtract(pos);
-                    a = a.negate();
-                    b = b.negate().add(size).subtract(BigInteger.ONE);
-                } else if (step instanceof CutCards) {
-                    var cut = (CutCards) step;
-                    var n = BigInteger.valueOf(cut.getNumCards());
+                pos = (pos.add(n)).mod(size);
+                b = b.add(n);
+            } else if (step instanceof DealIncrement) {
+                var deal = (DealIncrement) step;
+                var n = BigInteger.valueOf(deal.getIncrement());
+                var ninv = n.modInverse(size);
 
-                    if (n.compareTo(BigInteger.ZERO) < 0) {
-                        n = size.subtract(n.abs());
-                    }
-
-                    pos = (pos.add(n)).mod(size);
-                    b = b.add(n);
-                } else if (step instanceof DealIncrement) {
-                    var deal = (DealIncrement) step;
-                    var n = BigInteger.valueOf(deal.getIncrement());
-                    var ninv = n.modInverse(size);
-
-                    a = a.multiply(ninv);
-                    b = b.multiply(ninv);
-                    pos = (ninv.multiply(pos)).mod(size);
-                }
+                a = a.multiply(ninv);
+                b = b.multiply(ninv);
+                pos = (ninv.multiply(pos)).mod(size);
             }
+        }
 
-            a = a.mod(size);
-            b = b.mod(size);
+        a = a.mod(size);
+        b = b.mod(size);
 
-            if (pos.compareTo((a.multiply(oldPos).add(b)).mod(size)) != 0) {
-                throw new RuntimeException("Check failed");
-            }
-
-            iter = iter.add(BigInteger.ONE);
-
-            if (iter.compareTo(BigInteger.valueOf(10)) >= 0 || iter.compareTo(iters) == 0) {
-                break;
-            }
+        if (pos.compareTo((a.multiply(oldPos).add(b)).mod(size)) != 0) {
+            throw new RuntimeException("Check failed");
         }
 
         var pow = a.modPow(iters, size);
