7fee68753baf06d947bee944ecf2f1aacd62a7e0
Rich Heller <rellehr@gmail.com>
2024-10-12 21:51:32 -0500

    Use batch inserts

--- src/main/java/aoc/y2023/day2/Solver.java
+++ src/main/java/aoc/y2023/day2/Solver.java
@@ -1,12 +1,24 @@
 package aoc.y2023.day2;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.regex.Pattern;
 
 import aoc.y2023.Problem2023;
 
 public abstract class Solver extends Problem2023<Integer> {
+    static String createGameSQL = """
+            INSERT INTO "2023.day2".game (number) VALUES (?) RETURNING ID
+            """;
+    static String createRoundSQL = """
+            INSERT INTO "2023.day2".round (game_id, red, blue, green)
+            VALUES ((SELECT id FROM "2023.day2".game WHERE number = ?),?,?,?)
+            """;
+
+    static Pattern gameRegEx = Pattern.compile("Game (\\d+): (.*)");
+    static Pattern cubeRegEx = Pattern.compile("(\\d+) (red|green|blue)");
+
     abstract String getQuery();
 
     public Solver(String fileName, int exp) {
@@ -26,74 +38,60 @@ public abstract class Solver extends Problem2023<Integer> {
         }
     }
 
-    private int createGame(Connection conn, int number) throws Exception {
-        var sql = "INSERT INTO \"2023.day2\".game (number) VALUES (?) RETURNING ID";
+    private void createRounds(PreparedStatement ps, int gameId, String[] rounds) throws Exception {
+        for (var round : rounds) {
+            var cubes = round.split(",");
+            var red = 0;
+            var green = 0;
+            var blue = 0;
 
-        try (var ps = conn.prepareStatement(sql)) {
-            ps.setInt(1, number);
+            for (var cube : cubes) {
+                var cubeMatcher = cubeRegEx.matcher(cube.trim());
 
-            var result = ps.executeQuery();
+                if (!cubeMatcher.matches()) {
+                    throw new Exception("Invalid cube: " + cube);
+                }
 
-            result.next();
+                var count = Integer.parseInt(cubeMatcher.group(1));
+                var color = cubeMatcher.group(2).trim();
 
-            return result.getInt(1);
-        }
-    }
+                if ("red".equals(color)) {
+                    red = count;
+                } else if ("green".equals(color)) {
+                    green = count;
+                } else if ("blue".equals(color)) {
+                    blue = count;
+                }
+            }
 
-    private void createRound(Connection conn, int gameId, int red, int blue, int green) throws Exception {
-        var sql = "INSERT INTO \"2023.day2\".round (game_id, red, blue, green) VALUES (?,?,?,?)";
-
-        try (var ps = conn.prepareStatement(sql)) {
             ps.setInt(1, gameId);
             ps.setInt(2, red);
             ps.setInt(3, blue);
             ps.setInt(4, green);
-
-            ps.executeUpdate();
+            ps.addBatch();
         }
     }
 
     private void populateTables(Connection conn, List<String> lines) throws Exception {
-        var gameRegEx = Pattern.compile("Game (\\d+): (.*)");
-        var cubeRegEx = Pattern.compile("(\\d+) (red|green|blue)");
+        try (var gamePS = conn.prepareStatement(createGameSQL);
+                var roundPS = conn.prepareStatement(createRoundSQL)) {
+            for (var line : lines) {
+                var matcher = gameRegEx.matcher(line);
 
-        for (var line : lines) {
-            var matcher = gameRegEx.matcher(line);
-
-            if (!matcher.matches()) {
-                throw new Exception("Invalid input: " + line);
-            }
-
-            var gameId = Integer.parseInt(matcher.group(1));
-            var id = createGame(conn, gameId);
-
-            for (var round : matcher.group(2).split(";")) {
-                var cubes = round.split(",");
-                var red = 0;
-                var green = 0;
-                var blue = 0;
-
-                for (var cube : cubes) {
-                    var cubeMatcher = cubeRegEx.matcher(cube.trim());
-
-                    if (!cubeMatcher.matches()) {
-                        throw new Exception("Invalid cube: " + cube);
-                    }
-
-                    var count = Integer.parseInt(cubeMatcher.group(1));
-                    var color = cubeMatcher.group(2).trim();
-
-                    if ("red".equals(color)) {
-                        red = count;
-                    } else if ("green".equals(color)) {
-                        green = count;
-                    } else if ("blue".equals(color)) {
-                        blue = count;
-                    }
+                if (!matcher.matches()) {
+                    throw new Exception("Invalid input: " + line);
                 }
 
-                createRound(conn, id, red, blue, green);
+                var gameId = Integer.parseInt(matcher.group(1));
+
+                gamePS.setInt(1, gameId);
+                gamePS.addBatch();
+
+                createRounds(roundPS, gameId, matcher.group(2).split(";"));
             }
+
+            gamePS.executeBatch();
+            roundPS.executeBatch();
         }
     }
 
