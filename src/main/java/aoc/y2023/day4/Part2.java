97187b7952abf304ec48c41026cf407dac539a79
Rich Heller <rellehr@gmail.com>
2024-10-23 16:18:03 -0500

    Use DB for storing data

--- src/main/java/aoc/y2023/day4/Part2.java
+++ src/main/java/aoc/y2023/day4/Part2.java
@@ -1,16 +1,8 @@
 package aoc.y2023.day4;
 
 import java.sql.Connection;
-import java.util.HashMap;
 
 public class Part2 extends Solver {
-    private String countSQL = """
-            SELECT c.number AS id, COUNT(*) AS count
-                FROM "2023.day4".winning w
-                JOIN "2023.day4".card c ON c.id = w.card_id
-                JOIN "2023.day4".holding h ON h.card_id = w.card_id AND h.number = w.number
-                GROUP BY h.card_id, c.number
-            """;
     private String winsCountSQL = """
             WITH wins AS (
                 SELECT c.number AS id, COUNT(*) AS count
@@ -29,8 +21,14 @@ public class Part2 extends Solver {
             SET copies = copies + ?
             WHERE number = ?
             """;
+    private String sumCopiesSQL = """
+            SELECT SUM(copies) as copies FROM"2023.day4".card
+            """;
     private String cardNumberSQL = """
-            SELECT number, copies, wins FROM "2023.day4".card ORDER BY number ASC
+            SELECT number, wins FROM "2023.day4".card ORDER BY number ASC
+            """;
+    private String cardCopiesSQL = """
+            SELECT copies FROM "2023.day4".card WHERE number = ?
             """;
 
     public Part2(String fileName, int exp) {
@@ -42,34 +40,24 @@ public class Part2 extends Solver {
         computeWins(conn);
         updateCounts(conn);
 
-        printTable(conn);
-        // return sumCounts(counts);
-        return 0;
-    }
-
-    private void printTable(Connection conn) throws Exception {
-        try (var ps = conn.prepareStatement("SELECT number, copies, wins FROM \"2023.day4\".card")) {
-            var result = ps.executeQuery();
-
-            while (result.next()) {
-                System.out.println("***** " + result.getInt(1) + " " + result.getInt(2) + " " + result.getInt(3));
-            }
-        }
-    }
-
-    private void initCounts(Connection conn, HashMap<Integer, Integer> counts) throws Exception {
-        try (var ps = conn.prepareStatement(cardNumberSQL)) {
-            var result = ps.executeQuery();
-
-            while (result.next()) {
-                counts.put(result.getInt(1), 1);
-            }
-        }
+        return sumCounts(conn);
     }
 
     private void computeWins(Connection conn) throws Exception {
         try (var ps = conn.prepareStatement(winsCountSQL)) {
             ps.executeUpdate();
+        }
+    }
+
+    private int getCardCopies(Connection conn, int cardNumber) throws Exception {
+        try (var queryPS = conn.prepareStatement(cardCopiesSQL)) {
+            queryPS.setInt(1, cardNumber);
+
+            var result = queryPS.executeQuery();
+
+            result.next();
+
+            return result.getInt(1);
         }
     }
 
@@ -80,36 +68,25 @@ public class Part2 extends Solver {
 
             while (result.next()) {
                 var card = result.getInt(1);
-                var copies = result.getInt(2);
-                var wins = result.getInt(3);
+                var wins = result.getInt(2);
+                var copies = getCardCopies(conn, card);
 
-                System.out.println("***** UPDATE " + card + " " + wins);
                 for (var toAdd = card + 1; toAdd <= card + wins; toAdd++) {
                     updatePS.setInt(1, copies);
                     updatePS.setInt(2, toAdd);
-                    System.out.println("**** TOADD " + toAdd + " " + copies);
                     updatePS.executeUpdate();
                 }
             }
         }
-        // for (var card = 1; counts.containsKey(card); card++) {
-        //     var cardWins = wins.containsKey(card) ? wins.get(card) : 0;
-        //     var cardCount = counts.get(card);
-
-        //     for (var toAdd = card + 1; toAdd <= card + cardWins; toAdd++) {
-        //         var current = counts.get(toAdd);
-        //         counts.put(toAdd, current + cardCount);
-        //     }
-        // }
     }
 
-    private int sumCounts(HashMap<Integer, Integer> counts) {
-        int sum = 0;
+    private int sumCounts(Connection conn) throws Exception {
+        try (var ps = conn.prepareStatement(sumCopiesSQL)) {
+            var result = ps.executeQuery();
 
-        for (var card = 1; counts.containsKey(card); card++) {
-            sum += counts.get(card);
+            result.next();
+
+            return result.getInt(1);
         }
-
-        return sum;
     }
 }
