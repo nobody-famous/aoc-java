982ed7acaffd45a692221bc0eed2038df29ccabc
Rich Heller <rellehr@gmail.com>
2024-10-25 22:11:16 -0500

    Initial day5 setup

---
+++ src/main/java/aoc/y2023/day5/Part1.java
@@ -0,0 +1,17 @@
+package aoc.y2023.day5;
+
+import java.sql.Connection;
+import java.util.List;
+
+import aoc.y2023.Problem2023;
+
+public class Part1 extends Problem2023<Integer> {
+    public Part1(String fileName, int exp) {
+        super(fileName, exp);
+    }
+
+    @Override
+    public Integer run(Connection conn, List<String> lines) {
+        return 0;
+    }
+}
