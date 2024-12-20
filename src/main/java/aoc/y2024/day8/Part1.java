759a52dfcc02a833090fabaf3dda624aff18ea0d
Rich Heller <rellehr@gmail.com>
2024-12-20 12:52:08 -0600

    Day 8, part 2

--- src/main/java/aoc/y2024/day8/Part1.java
+++ src/main/java/aoc/y2024/day8/Part1.java
@@ -1,81 +1,19 @@
 package aoc.y2024.day8;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 
 import aoc.utils.Grid;
-import aoc.utils.Problem;
 import aoc.utils.Pair;
 import aoc.utils.geometry.Point;
 
-public class Part1 extends Problem<Integer> {
+public class Part1 extends Solver {
     public Part1(String fileName, int exp) {
         super(fileName, exp);
     }
 
-    private Grid parseInput(List<String> lines) {
-        var grid = new Grid(lines.size(), lines.get(0).length());
-
-        for (var row = 0; row < lines.size(); row += 1) {
-            var line = lines.get(row);
-
-            for (var col = 0; col < line.length(); col += 1) {
-                grid.set(row, col, line.charAt(col));
-            }
-        }
-
-        return grid;
-    }
-
-    private void addFrequency(HashMap<Character, List<Point>> map, char key, Point pt) {
-        if (!map.containsKey(key)) {
-            map.put(key, new ArrayList<Point>());
-        }
-
-        map.get(key).add(pt);
-    }
-
-    private HashMap<Character, List<Point>> getFrequencies(Grid grid) {
-        var frequencies = new HashMap<Character, List<Point>>();
-
-        for (var row = 0; row < grid.getRows(); row += 1) {
-            for (var col = 0; col < grid.getCols(); col += 1) {
-                var ch = grid.get(row, col);
-
-                if (ch != '.') {
-                    addFrequency(frequencies, ch, new Point(row, col));
-                }
-            }
-        }
-
-        return frequencies;
-    }
-
-    private List<Pair<Point>> pairsFromList(List<Point> pts) {
-        var pairs = new ArrayList<Pair<Point>>();
-
-        for (var start = 0; start < pts.size() - 1; start += 1) {
-            for (var index = start + 1; index < pts.size(); index += 1) {
-                pairs.add(new Pair<Point>(pts.get(start), pts.get(index)));
-            }
-        }
-
-        return pairs;
-    }
-
-    private List<Pair<Point>> getPairs(HashMap<Character, List<Point>> map) {
-        var pairs = new ArrayList<Pair<Point>>();
-
-        for (var pts : map.values()) {
-            pairs.addAll(pairsFromList(pts));
-        }
-
-        return pairs;
-    }
-
-    private HashSet<Point> createAntinodes(List<Pair<Point>> pairs, Grid grid) {
+    @Override
+    protected HashSet<Point> createAntinodes(List<Pair<Point>> pairs, Grid grid) {
         var nodes = new HashSet<Point>();
 
         for (var pair : pairs) {
@@ -95,15 +33,5 @@ public class Part1 extends Problem<Integer> {
         }
 
         return nodes;
-    }
-
-    @Override
-    public Integer run(List<String> lines) {
-        var grid = parseInput(lines);
-        var frequencies = getFrequencies(grid);
-        var pairs = getPairs(frequencies);
-        var nodes = createAntinodes(pairs, grid);
-
-        return nodes.size();
     }
 }
