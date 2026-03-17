bcfd3fee6bcbaca47cf9b20922ed78ef9db1ffbb
Rich Heller <rellehr@gmail.com>
2026-03-17 14:07:35 -0500

    Part 2 solution

--- src/main/java/aoc/y2024/day11/Part1.java
+++ src/main/java/aoc/y2024/day11/Part1.java
@@ -1,50 +1,7 @@
 package aoc.y2024.day11;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import aoc.utils.AocProblem;
-
-public class Part1 implements AocProblem<Integer> {
-    private static final int MULTIPLIER = 2024;
-
-    private int countDigits(long number) {
-        return (int) Math.log10(number) + 1;
-    }
-
-    private List<Long> splitStone(int digits, long number) {
-        var base = (long) Math.pow(10, digits);
-
-        return List.of(number / base, number % base);
-    }
-
-    private List<Long> playStone(long number) {
-        var numDigits = countDigits(number);
-
-        if (number == 0) {
-            return List.of(1L);
-        } else if ((numDigits % 2) == 0) {
-            return splitStone(numDigits / 2, number);
-        } else {
-            return List.of(number * MULTIPLIER);
-        }
-    }
-
-    private List<Long> playStone(long number, int count) {
-        var list = List.of(number);
-
-        for (var iter = 0; iter < count; iter++) {
-            list = list.stream().flatMap(stone -> playStone(stone).stream()).toList();
-        }
-
-        return list;
-    }
-
-    @Override
-    public Integer solve(List<String> lines) {
-        var input = Arrays.stream(lines.get(0).split(" ")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-
-        return input.stream().flatMap(stone -> playStone(stone, 25).stream()).toList().size();
+public class Part1 extends Solver {
+    public Part1() {
+        super(25);
     }
 }
---
+++ src/main/java/aoc/y2024/day11/Part2.java
@@ -0,0 +1,7 @@
+package aoc.y2024.day11;
+
+public class Part2 extends Solver {
+    public Part2() {
+        super(75);
+    }
+}
