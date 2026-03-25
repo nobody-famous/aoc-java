4f1599d79a36a8de871c4faaea6e6369308be4a8
Rich Heller <rellehr@gmail.com>
2026-03-24 23:12:32 -0500

    Part 2 solution

--- src/main/java/aoc/y2024/day12/Part1.java
+++ src/main/java/aoc/y2024/day12/Part1.java
@@ -1,99 +1,21 @@
 package aoc.y2024.day12;
 
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
-import aoc.utils.AocProblem;
-import aoc.utils.Grid;
-
-public class Part1 implements AocProblem<Integer> {
-    private record Region(int area, int fences) {
-    }
-
+public class Part1 extends Solver {
     @Override
-    public Integer solve(List<String> lines) {
-        var grid = Grid.parse(lines);
-        var regions = findRegions(grid);
-
-        return regions.stream().map(r -> r.area * r.fences).mapToInt(Integer::intValue).sum();
+    protected int calculatePrice(List<Region> regions) {
+        return regions
+                .stream()
+                .map(r -> r.area() * sumFences(r))
+                .mapToInt(Integer::intValue)
+                .sum();
     }
 
-    private List<Region> findRegions(Grid grid) {
-        var regions = new ArrayList<Region>();
-        var used = new boolean[grid.getRows()][grid.getCols()];
-
-        while (!allUsed(used)) {
-            var start = findStart(grid, used);
-
-            regions.add(findRegion(grid, start, used));
-        }
-
-        return regions;
-    }
-
-    private Region findRegion(Grid grid, Grid.Loc start, boolean[][] used) {
-        var target = grid.get(start);
-        var toCheck = new HashSet<Grid.Loc>();
-        var diffs = List.of(
-                new Grid.Loc(-1, 0),
-                new Grid.Loc(1, 0),
-                new Grid.Loc(0, -1),
-                new Grid.Loc(0, 1));
-        var area = 0;
-        var fences = 0;
-
-        toCheck.add(start);
-
-        while (!toCheck.isEmpty()) {
-            var newToCheck = new HashSet<Grid.Loc>();
-
-            for (var loc : toCheck) {
-                area++;
-                used[loc.row()][loc.col()] = true;
-
-                var neighbors = diffs.stream().map(diff -> new Grid.Loc(loc.row() + diff.row(), loc.col() + diff.col())).toList();
-
-                for (var neighbor : neighbors) {
-                    if (grid.onMap(neighbor) && grid.get(neighbor) == target && used[neighbor.row()][neighbor.col()]) {
-                        continue;
-                    }
-
-                    if (grid.get(neighbor) == target) {
-                        newToCheck.add(neighbor);
-                    } else {
-                        fences++;
-                    }
-                }
-            }
-
-            toCheck = newToCheck;
-        }
-
-        return new Region(area, fences);
-    }
-
-    private boolean allUsed(boolean[][] used) {
-        for (var row = 0; row < used.length; row++) {
-            for (var col = 0; col < used[row].length; col++) {
-                if (!used[row][col]) {
-                    return false;
-                }
-            }
-        }
-
-        return true;
-    }
-
-    private Grid.Loc findStart(Grid grid, boolean[][] used) {
-        for (var row = 0; row < used.length; row++) {
-            for (var col = 0; col < used[row].length; col++) {
-                if (!used[row][col]) {
-                    return new Grid.Loc(row, col);
-                }
-            }
-        }
-
-        throw new RuntimeException("did not find starting point");
+    private static int sumFences(Region region) {
+        return region.northFences().size()
+                + region.southFences().size()
+                + region.eastFences().size()
+                + region.westFences().size();
     }
 }
--- src/test/java/aoc/y2024/day12/UnitTests.java
+++ src/test/java/aoc/y2024/day12/UnitTests.java
@@ -31,11 +31,33 @@ public class UnitTests {
             "MIIIIIJJEE",
             "MIIISIJEEE",
             "MMMISSJEEE");
+    private static final List<String> example4 = List.of(
+            "EEEEE",
+            "EXXXX",
+            "EEEEE",
+            "EXXXX",
+            "EEEEE");
+    private static final List<String> example5 = List.of(
+            "AAAAAA",
+            "AAABBA",
+            "AAABBA",
+            "ABBAAA",
+            "ABBAAA",
+            "AAAAAA");
 
     @Test
     void testPart1() {
         assertEquals(140, new Part1().solve(example1));
         assertEquals(772, new Part1().solve(example2));
         assertEquals(1930, new Part1().solve(example3));
+    }
+
+    @Test
+    void testPart2() {
+        assertEquals(80, new Part2().solve(example1));
+        assertEquals(436, new Part2().solve(example2));
+        assertEquals(1206, new Part2().solve(example3));
+        assertEquals(236, new Part2().solve(example4));
+        assertEquals(368, new Part2().solve(example5));
     }
 }
